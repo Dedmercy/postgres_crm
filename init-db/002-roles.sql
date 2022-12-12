bd33dde41764e76dc4e1c2509a4e3877c5bc3d33
Dedmercy <Dedmercy@yandex.ru>
2022-12-12 12:43:06 +0300

    added roles

---
+++ init-db/002-roles.sql
@@ -0,0 +1,12 @@
+
+-- Серверная роль
+CREATE ROLE backend WITH
+	LOGIN,
+	PASSWORD "Rkahwhx5rbY8",
+	CREATEROLE;
+
+-- Роль клиент
+CREATE ROLE client;
+
+-- Роль исполнителя
+CREATE ROLE freelancer;
